27809a931a1be3371706375be2d618fd58988ac2
Alfredo <21030761@itcelaya.edu.mx>
2023-07-15 17:31:44 -0600

    Restructure App

--- src/App/index.js
+++ src/App/index.js
@@ -1,13 +1,39 @@
 import React from 'react';
+
+import { TodoHeader } from '../TodoHeader';
+import { TodoCounter } from '../TodoCounter';
+import { TodoSearch } from '../TodoSearch';
+
 import { TodoProvider } from '../TodoContext';
-import { AppUI } from './AppUI';
+import { TodoContext } from '../TodoContext';
+import { TodoList } from '../TodoList';
+import { TodoItem } from '../TodoItem';
+import { TodosError } from '../TodosError';
+import { TodosLoading } from '../TodosLoading';
+import { EmptyTodos } from '../EmptyTodos';
+import { TodoForm } from '../TodoForm';
+import { CreateTodoButton } from '../CreateTodoButton';
+import { Modal } from '../Modal';
 
 
 function App() {
   return (
-    <TodoProvider>
-      <AppUI />
-    </TodoProvider>
+    <>
+      <TodoHeader>
+        <TodoCounter/>
+        <TodoSearch/>
+      </TodoHeader>
+
+      <TodoList>
+        {/* TODO ITEMS WOULD BE RENDER HERE */}
+      </TodoList>
+
+      <CreateTodoButton/>
+
+      <Modal>
+        <TodoForm/>
+      </Modal>
+    </>
   );
 }
 
