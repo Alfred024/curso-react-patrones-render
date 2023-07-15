c35d4bccb42496daa7ae0bd8bfa4ead49d0921de
Alfredo <21030761@itcelaya.edu.mx>
2023-07-15 17:44:18 -0600

    Consuming Todos data from useTodos custom hook

--- src/App/index.js
+++ src/App/index.js
@@ -1,20 +1,42 @@
 import React from 'react';
 
+//Header components
 import { TodoHeader } from '../TodoHeader';
 import { TodoCounter } from '../TodoCounter';
 import { TodoSearch } from '../TodoSearch';
 
+//List components
 import { TodoList } from '../TodoList';
 import { TodoItem } from '../TodoItem';
 import { TodosError } from '../TodosError';
 import { TodosLoading } from '../TodosLoading';
 import { EmptyTodos } from '../EmptyTodos';
+
+//Modal components
 import { TodoForm } from '../TodoForm';
 import { CreateTodoButton } from '../CreateTodoButton';
 import { Modal } from '../Modal';
 
+//Custom hooks
+import { useTodos } from '../utils/useTodos';
+
 
 function App() {
+  const {
+    loading,
+    error,
+    totalTodos,
+    completedTodos,
+    searchValue,
+    setSearchValue,
+    searchedTodos,
+    addTodo,
+    completeTodo,
+    deleteTodo,
+    openModal,
+    setOpenModal,    
+  } = useTodos();
+
   return (
     <>
       <TodoHeader>
@@ -22,7 +44,9 @@ function App() {
         <TodoSearch/>
       </TodoHeader>
 
-      <TodoList>
+      <TodoList
+
+      >
         {/* TODO ITEMS WOULD BE RENDER HERE */}
       </TodoList>
 
